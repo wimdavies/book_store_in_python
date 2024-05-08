1b0206f1817861a0998c8e4f0e18d28b298e8c04
wimdavies <william.johnston.davies@gmail.com>
2024-05-08 11:25:39 +0100

    Set up for book_store

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ connection = DatabaseConnection()
 connection.connect()
 
 # Seed with some seed data
-connection.seed("seeds/music_library.sql")
+connection.seed("seeds/book_store.sql")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+# # Retrieve all artists
+# artist_repository = ArtistRepository(connection)
+# artists = artist_repository.all()
 
-# List them out
-for artist in artists:
-    print(artist)
+# # List them out
+# for artist in artists:
+#     print(artist)
